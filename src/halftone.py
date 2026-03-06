47b66fc68b3f672bad00dabed2d6cbd8a75fa0e9
Aleena Treesa Leejoy <116470292+Nothing-atl@users.noreply.github.com>
2026-03-05 17:07:30 -0800

    baseline some comments added

--- src/halftone.py
+++ src/halftone.py
@@ -10,7 +10,7 @@ BAYER_4 = (1/16.0) * np.array([
 def ordered_dither(gray):
     img = gray.astype(np.float32) / 255.0
     h, w = img.shape
-    tiled = np.tile(BAYER_4, (h//4 + 1, w//4 + 1))
-    tiled = tiled[:h, :w]
-    out = (img > tiled) * 255
+    tiled = np.tile(BAYER_4, (h//4 + 1, w//4 + 1))# Repeat the Bayer matrix so it covers the whole image.The matrix repeats every 4x4 pixels
+    tiled = tiled[:h, :w] # Crop the tiled matrix so it matches the exact image size
+    out = (img > tiled) * 255.  # Compare each pixel intensity with the threshold matrix.If pixel > threshold → white (255). If pixel ≤ threshold → black (0)
     return out.astype(np.uint8)
--- src/optical_flow.py
+++ src/optical_flow.py
@@ -1,5 +1,8 @@
 import cv2
 
+# Compute dense optical flow between two frames using Farneback's method.
+# Returns motion vectors (dx,dy) for each pixel indicating how the image. moved from the previous frame to the current frame.
+# the format is: cv2.calcOpticalFlowFarneback( prev, next, flow, pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags )
 def compute_flow(prev, curr):
     flow = cv2.calcOpticalFlowFarneback(prev, curr,None, 0.5,3,15,3, 5,1.2, 0)
     return flow
